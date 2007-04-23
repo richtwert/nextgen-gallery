d0fd3572417e50b685011baae9923a7962f7f86b
alex.cologne@c3e66f6f-232e-0410-82ea-bf51f2194cab <none@none>
2007-04-23 20:21:56 +0000

    --HG-- extra : convert_revision : svn%3Ac3e66f6f-232e-0410-82ea-bf51f2194cab/trunk%405

--- nggallery.php
+++ nggallery.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Plugin Name: NextGEN Gallery
-Plugin URI: http://alexrabe.boelinger.com/
-Description: A NextGENeration Photo gallery for the WEB2.0(beta). At the moment onyl poor Web1.0 :-(
+Plugin URI: http://alexrabe.boelinger.com/?page_id=80
+Description: A NextGENeration Photo gallery for the WEB2.0(beta). At the moment only poor Web1.0 :-(
 Author: Alex Rabe
 Version: 0.33a
 
