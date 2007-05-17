edeb445b03f5f993e58960061a064f1c7e971d25
alex.cologne@c3e66f6f-232e-0410-82ea-bf51f2194cab <none@none>
2007-05-17 17:07:34 +0000

    Open Version 0.42
    --HG--
    extra : convert_revision : svn%3Ac3e66f6f-232e-0410-82ea-bf51f2194cab/trunk%4044

--- nggallery.php
+++ nggallery.php
@@ -4,7 +4,7 @@ Plugin Name: NextGEN Gallery
 Plugin URI: http://alexrabe.boelinger.com/?page_id=80
 Description: A NextGENeration Photo gallery for the WEB2.0(beta).
 Author: NextGEN DEV-Team
-Version: 0.41a
+Version: 0.42a
 
 Author URI: http://alexrabe.boelinger.com/
 
@@ -42,7 +42,7 @@ global $wpdb, $wp_version;
 if (version_compare($wp_version, '2.1', '>=')) {
 
 // Version and path to check version
-define('NGGVERSION', "0.41");
+define('NGGVERSION', "0.42");
 define('NGGURL', "http://nextgen.boelinger.com/version.php");
 
 // define URL
