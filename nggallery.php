5e26008f97241cbf179843801476fb7612082280
alex.cologne@c3e66f6f-232e-0410-82ea-bf51f2194cab <none@none>
2008-01-06 18:24:31 +0000

    --HG-- extra : convert_revision : svn%3Ac3e66f6f-232e-0410-82ea-bf51f2194cab/trunk%40104

--- nggallery.php
+++ nggallery.php
@@ -4,7 +4,7 @@ Plugin Name: NextGEN Gallery
 Plugin URI: http://alexrabe.boelinger.com/?page_id=80
 Description: A NextGENeration Photo gallery for the WEB2.0(beta).
 Author: NextGEN DEV-Team
-Version: 0.80.3a
+Version: 0.80.4a
 
 Author URI: http://alexrabe.boelinger.com/
 
@@ -94,14 +94,22 @@ function nggallery_init ()
 	load_plugin_textdomain('nggallery','wp-content/plugins/' . NGGFOLDER.'/lang');
 }
 
-// Load admin panel
-include_once (dirname (__FILE__)."/ngginstall.php");
-include_once (dirname (__FILE__)."/nggfunctions.php");
-include_once (dirname (__FILE__)."/admin/admin.php");
+// Load the admin panel
+if (is_admin()) {
+	include_once (dirname (__FILE__)."/ngginstall.php");
+	include_once (dirname (__FILE__)."/admin/admin.php");
+	include_once (dirname (__FILE__)."/tinymce/tinymce.php");
+} else {
+// Load the gallery generator
+	include_once (dirname (__FILE__)."/nggfunctions.php");
+}
 
 // load gallery class
 require_once (dirname (__FILE__).'/lib/nggallery.lib.php');
 $nggallery = new nggallery();
+
+// Add rewrite rules
+$nggRewrite = new nggRewrite();
 
 // add javascript to header
 add_action('wp_head', 'ngg_addjs', 1);
@@ -145,8 +153,11 @@ register_deactivation_hook(NGGFOLDER.'/nggallery.php','ngg_deinstall');
 
 // init tables in wp-database if plugin is activated
 function ngg_install() {
+	global $nggRewrite;
 	// Check for admin role
 	nggallery_install();
+	// Flush ReWrite rules
+	$nggRewrite->flush();
 }
 
 function ngg_deinstall() {
@@ -160,128 +171,6 @@ add_filter('the_excerpt', 'searchnggallerytags');
 
 // Content Filters
 add_filter('ngg_gallery_name', 'sanitize_title');
-
-
-//#################################################################
-// add action/filter for the upload tab 
-add_action('upload_files_ngg_gallery', 'ngg_action_upload_Tab');
-add_filter('wp_upload_tabs', 'ngg_wp_upload_tabs');
-
-function ngg_action_upload_Tab() {
-	// execute when click on the tab
-	global $style;
-	if ( 'inline' == $style )
-		wp_enqueue_script('nggadmintab', NGGALLERY_URLPATH .'js/nggadmintab-js.php', array('prototype'), '0.6');
-}
-
-function ngg_wp_upload_tabs ($array) {
-	
-	global $wpdb;
-	
-    /* 
-    0 => tab display name, 
-    1 => required cap / role, 
-    2 => function that produces tab content, 
-    3 => total number objects OR array(total, objects per page), 
-    4 => add_query_args
-	*/
-	include_once (dirname (__FILE__)."/nggadmintab.php");
-
-	// Create navigation
-	$total = 1;
-	if ($_GET['select_gal']){
-		$galleryID = $_GET['select_gal'];
-		$total = $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->nggpictures WHERE galleryid = '$galleryID'");
-	}
-
-	$tab = array(
-            'ngg_gallery' => array(__('Gallery','nggallery'), 'NextGEN Use TinyMCE', 'ngg_upload_tab_content', array($total, 10))
-    );
-
-    return array_merge($array,$tab);
-}
-//#################################################################
-// TinyMCE Button Integration
-
-// Load the Script for the Button
-function insert_nextgen_script() {	
- 
- 	//TODO: Do with WP2.1 Script Loader
- 	// Thanks for this idea to www.jovelstefan.de
-	echo "\n"."
-	<script type='text/javascript'> 
-		function ngg_buttonscript()	{ 
-		if(window.tinyMCE) {
-
-			var template = new Array();
-	
-			template['file'] = '".NGGALLERY_URLPATH."nggbutton.php';
-			template['width'] = 360;
-			template['height'] = 210;
-	
-			args = {
-				resizable : 'no',
-				scrollbars : 'no',
-				inline : 'yes'
-			};
-	
-			tinyMCE.openWindow(template, args);
-			return true;
-		} 
-	} 
-	</script>"; 
-	return;
-}
-
-function ngg_addbuttons() {
- 
-	global $wp_db_version;
-
-	// Don't bother doing this stuff if the current user lacks permissions
-	if ( !current_user_can('edit_posts') && !current_user_can('edit_pages') ) return;
-	
-	// Check for NextGEN capability
-	if ( !current_user_can('NextGEN Use TinyMCE') ) return;
-	 
-	// Add only in Rich Editor mode
-	if ( get_user_option('rich_editing') == 'true') {
-	 
-	// add the button for wp21 in a new way
-		add_filter("mce_plugins", "nextgen_button_plugin", 5);
-		add_filter('mce_buttons', 'nextgen_button', 5);
-		add_action('tinymce_before_init','nextgen_button_script');
-		}
-}
-
-// used to insert button in wordpress 2.1x editor
-function nextgen_button($buttons) {
-
-	array_push($buttons, "separator", "NextGEN");
-	return $buttons;
-
-}
-
-// Tell TinyMCE that there is a plugin (wp2.1)
-function nextgen_button_plugin($plugins) {    
-
-	array_push($plugins, "-NextGEN");    
-	return $plugins;
-}
-
-// Load the TinyMCE plugin : editor_plugin.js (wp2.1)
-function nextgen_button_script() {	
- 
- 	$pluginURL =  NGGALLERY_URLPATH.'js/';
-	echo 'tinyMCE.loadPlugin("NextGEN", "'.$pluginURL.'");' . "\n"; 
-	return;
-}
-
-// init process for button control
-add_action('init', 'ngg_addbuttons');
-add_action('edit_page_form', 'insert_nextgen_script');
-add_action('edit_form_advanced', 'insert_nextgen_script');
-
-//#################################################################
 
 } else {
 	add_action('admin_notices', create_function('', 'echo \'<div id="message" class="error fade"><p><strong>' . __('Sorry, NextGEN Gallery works only under WordPress 2.1 or higher',"nggallery") . '</strong></p></div>\';'));
