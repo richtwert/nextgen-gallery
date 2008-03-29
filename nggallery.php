fdaaf654a99a5f856e3fab3bf34528780686cda7
alex.cologne@c3e66f6f-232e-0410-82ea-bf51f2194cab <none@none>
2008-03-29 07:12:52 +0000

    Bugfix for RC3 NextGEN should have higher priority than 9
    ### YES IGNORE MY PLUGIN ###
    
    --HG--
    extra : convert_revision : svn%3Ac3e66f6f-232e-0410-82ea-bf51f2194cab/trunk%40185

--- nggallery.php
+++ nggallery.php
@@ -117,15 +117,11 @@ if (is_admin()) {
 	// Load the gallery generator
 	include_once (dirname (__FILE__)."/nggfunctions.php");
 	
-	// required in WP 2.5, NextGEN should have higher priority
-	if (IS_WP25) {	
-		remove_filter('the_content', 'do_shortcode');
-		add_filter('the_content', 'do_shortcode', 20);
-	}
-		
 	// Action calls for all functions 
-	add_filter('the_content', 'searchnggallerytags', 10);
-	add_filter('the_excerpt', 'searchnggallerytags', 10);
+	// required in WP 2.5, NextGEN should have higher priority than 9
+	// see also http://trac.wordpress.org/ticket/6436 
+	add_filter('the_content', 'searchnggallerytags', 6);
+	add_filter('the_excerpt', 'searchnggallerytags', 6);
 }
 
 // Load tinymce button 
