fa527b47a4f74bb8c532ec4185917c61438aee48
alex.cologne@c3e66f6f-232e-0410-82ea-bf51f2194cab <none@none>
2008-07-10 19:51:10 +0000

    is_ssl() works only under wp2.6
    --HG--
    extra : convert_revision : svn%3Ac3e66f6f-232e-0410-82ea-bf51f2194cab/trunk%40234

--- admin/upload.php
+++ admin/upload.php
@@ -13,10 +13,15 @@ if (file_exists($root.'/wp-load.php')) {
 }
 
 // Flash often fails to send cookies with the POST or upload, so we need to pass it in GET or POST instead
-if ( is_ssl() && empty($_COOKIE[SECURE_AUTH_COOKIE]) && !empty($_REQUEST['auth_cookie']) )
-	$_COOKIE[SECURE_AUTH_COOKIE] = $_REQUEST['auth_cookie'];
-elseif ( empty($_COOKIE[AUTH_COOKIE]) && !empty($_REQUEST['auth_cookie']) )
-	$_COOKIE[AUTH_COOKIE] = $_REQUEST['auth_cookie'];
+if (function_exists('is_ssl')) {
+	if ( is_ssl() && empty($_COOKIE[SECURE_AUTH_COOKIE]) && !empty($_REQUEST['auth_cookie']) )
+		$_COOKIE[SECURE_AUTH_COOKIE] = $_REQUEST['auth_cookie'];
+	elseif ( empty($_COOKIE[AUTH_COOKIE]) && !empty($_REQUEST['auth_cookie']) )
+		$_COOKIE[AUTH_COOKIE] = $_REQUEST['auth_cookie'];
+} else {
+	if ( empty($_COOKIE[AUTH_COOKIE]) && !empty($_REQUEST['auth_cookie']) )
+		$_COOKIE[AUTH_COOKIE] = $_REQUEST['auth_cookie'];
+}
 
 // don't ask me why, sometimes needed, taken from wp core
 unset($current_user);
