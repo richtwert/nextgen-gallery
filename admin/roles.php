20468677bea71d03b2e540bcaeab0e8d5b0254b2
alex.cologne@c3e66f6f-232e-0410-82ea-bf51f2194cab <none@none>
2010-01-31 20:25:24 +0000

    Link to Capability Manager instead of Role Manager
    --HG--
    extra : convert_revision : svn%3Ac3e66f6f-232e-0410-82ea-bf51f2194cab/trunk%40682

--- admin/roles.php
+++ admin/roles.php
@@ -26,7 +26,7 @@ if ( isset($_POST['update_cap']) ) {
 	<div class="wrap">
 	<h2><?php _e('Roles / capabilities', 'nggallery') ;?></h2>
 	<p><?php _e('Select the lowest role which should be able to access the follow capabilities. NextGEN Gallery supports the standard roles from WordPress.', 'nggallery') ?> <br />
-	   <?php _e('For a more flexible user management you can use the', 'nggallery') ?> <a href="http://www.im-web-gefunden.de/wordpress-plugins/role-manager/" target="_blank">Role Manager</a>.</p>
+	   <?php _e('For a more flexible user management you can use the', 'nggallery') ?> <a href="http://wordpress.org/extend/plugins/capsman/" target="_blank">Capability Manager</a>.</p>
 	<form name="addroles" id="addroles" method="POST" accept-charset="utf-8" >
 		<?php wp_nonce_field('ngg_addroles') ?>
 			<table class="form-table"> 
