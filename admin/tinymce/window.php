c39ad1ad3713c6da9deeaf2711aacd55eeafb7a4
alex.cologne@c3e66f6f-232e-0410-82ea-bf51f2194cab <none@none>
2010-02-20 19:55:33 +0000

    Not OR must be AND for the cap check
    --HG--
    extra : convert_revision : svn%3Ac3e66f6f-232e-0410-82ea-bf51f2194cab/trunk%40694

--- admin/tinymce/window.php
+++ admin/tinymce/window.php
@@ -4,7 +4,7 @@
 require_once( dirname( dirname( dirname(__FILE__) ) ) . '/ngg-config.php');
 
 // check for rights
-if ( !current_user_can('edit_pages') || !current_user_can('edit_posts') ) 
+if ( !current_user_can('edit_pages') && !current_user_can('edit_posts') ) 
 	wp_die(__("You are not allowed to be here"));
 
 global $wpdb, $nggdb;
