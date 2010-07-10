4322f8a4c779c846bc3a071eee446416b96f2908
alex.cologne@c3e66f6f-232e-0410-82ea-bf51f2194cab <none@none>
2010-07-10 11:53:48 +0000

    Second pass of jQuery Cycle. Still not perfect
    --HG--
    extra : convert_revision : svn%3Ac3e66f6f-232e-0410-82ea-bf51f2194cab/trunk%40751

--- js/ngg.slideshow.js
+++ js/ngg.slideshow.js
@@ -1,20 +1,42 @@
 function nggStartSlideshow( obj, id, width, height) { 
     
     var obj = '#' + obj;
-
-	var url = 'http://localhost/wpdev/index.php?callback=json&format=json&method=gallery&id=' + id;
+    var stack = [];
+	var url = 'http://localhost/wpdev/index.php?callback=json&api_key=true&format=json&method=gallery&id=' + id;
 
 	jQuery.getJSON(url, function(r){
 		if (r.stat == "ok"){
 		  
-            for ( img in r.images) {
+            var i = 0; 
+                            
+            for (img in r.images) {
+                i++;
 				var photo = r.images[img];
 				var alt   = photo['alttext'].replace(/<("[^"]*"|'[^']*'|[^'">])*>/gi,"");
 				var title = photo['description'].replace(/<("[^"]*"|'[^']*'|[^'">])*>/gi,"");
 				var src   = decodeURI( photo['imageURL'] );
                 //populate images
-			    jQuery( obj ).append( "<div class='ngg-slide'><img src='" + src + "' width='" + width + "' alt='" + alt + "'/></div>" );                
+			    jQuery( obj ).append( "<img src='" + src + "' width='" + width + "' alt='" + alt + "'/>" );
+                if (i == 2)
+                    break;
+          
             }
+            
+            // preload images into an array; we will preload id 3 and higher
+            for ( img in r.images ) { 
+                var new_img = new Image( );
+                var photo = r.images[img];
+                
+                new_img.src = decodeURI( photo['imageURL'] );
+                new_img.width = width;
+                new_img.height = height;
+                new_img.alt = photo['alttext'].replace(/<("[^"]*"|'[^']*'|[^'">])*>/gi,"");
+                new_img.title = photo['description'].replace(/<("[^"]*"|'[^']*'|[^'">])*>/gi,"");
+                
+                jQuery( new_img ).bind('load', function() {
+                    stack.push(this); 
+                }); 
+            } 
             
             // Setup the max-height
             jQuery( obj + ' .ngg-slideshow div img' ).each( function () {
@@ -30,10 +52,20 @@ function nggStartSlideshow( obj, id, width, height) {
             	// Start the cycle plugin
             	jQuery( obj ).cycle( {
             		fx: 	'fade',
-            		height: height
+            		height: height,
+                    before: jCycle_onBefore 
             	});
             });            
             
 		}
 	});
+
+    // add images to slideshow 
+    function jCycle_onBefore(curr, next, opts) { 
+        if (opts.addSlide) // <-- important! 
+            while(stack.length) {
+                opts.addSlide(stack.pop());
+            }  
+    }; 
+
 }
