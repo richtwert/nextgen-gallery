fc158d770ea571afc76c2d6a575b062c45a02b54
alex.cologne@c3e66f6f-232e-0410-82ea-bf51f2194cab <none@none>
2011-01-15 09:27:30 +0000

    Update changelog and typo fixes
    --HG--
    extra : convert_revision : svn%3Ac3e66f6f-232e-0410-82ea-bf51f2194cab/trunk%40899

--- lib/xmlrpc.php
+++ lib/xmlrpc.php
@@ -218,7 +218,7 @@ class nggXMLRPC{
 	 * Method "ngg.deleteImage"
 	 * Delete a Image from the database and gallery
 	 * 
-	 * @since 1.7.4
+	 * @since 1.7.3
 	 * 
 	 * @param array $args Method parameters.
 	 * 			- int blog_id
