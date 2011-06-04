12e3d307dfe79de9fe70a543458353a23abcc42e
alex.cologne@c3e66f6f-232e-0410-82ea-bf51f2194cab <none@none>
2011-06-04 22:11:52 +0000

    Do no check if custom stylesheet folder
    --HG--
    extra : convert_revision : svn%3Ac3e66f6f-232e-0410-82ea-bf51f2194cab/trunk%40949

--- lib/core.php
+++ lib/core.php
@@ -3,7 +3,7 @@
 * Main PHP class for the WordPress plugin NextGEN Gallery
 * 
 * @author 		Alex Rabe 
-* @copyright 	Copyright 2007 - 2009
+* @copyright 	Copyright 2007 - 2011
 * 
 */
 class nggGallery {
@@ -311,10 +311,10 @@ class nggGallery {
 	 */
 	function get_theme_css_file() {
 	   
-  		// allow other plugins to include a stylesheet
+  		// allow other plugins to include a custom stylesheet
 		$stylesheet = apply_filters( 'ngg_load_stylesheet', false );
         
-		if ( ( $stylesheet != false ) &&  file_exists ($stylesheet) )
+		if ( $stylesheet !== false )
 			return ( $stylesheet );
 		elseif ( file_exists (STYLESHEETPATH . '/nggallery.css') )
 			return get_stylesheet_directory_uri() . '/nggallery.css';
@@ -538,5 +538,4 @@ class nggGallery {
         return false;    
     }
 }
-
 ?>
