923fb5c0f1ca424c36d205d97efc176ead75de5b
Elia Sarti <elia@photocrati.com>
2011-12-30 00:01:00 +0100

    Continued renaming of module folders.
    Renamed 00_simple_html_dom module to simple_html_dom.
    
    --HG--
    branch : module-loader-new

---
+++ lib/modules/simple_html_dom/module.simple_html_dom.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once('simplehtmldom/simple_html_dom.php');
+class M_Simple_Html_Dom extends C_Base_Module
+{
+    function initialize()
+    {
+        parent::initialize(
+        		'photocrati-simple_html_dom',
+            'Simple HTML Dom',
+            'Provides the simple_html_dom utility for other modules to use',
+            '0.1',
+            'http://www.photocrati.com',
+            'Photocrati Media',
+            'http://www.photocrati.com'
+        );
+    }
+}
+
+new M_Simple_Html_Dom();
