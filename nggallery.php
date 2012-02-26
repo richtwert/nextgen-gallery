ca6df1181aa8d261ccdc7889c35539c735045635
alex.cologne@c3e66f6f-232e-0410-82ea-bf51f2194cab <none@none>
2012-02-26 10:42:42 +0000

    Release of 1.9.3
    --HG--
    extra : convert_revision : svn%3Ac3e66f6f-232e-0410-82ea-bf51f2194cab/trunk%401074

--- nggallery.php
+++ nggallery.php
@@ -4,11 +4,11 @@ Plugin Name: NextGEN Gallery
 Plugin URI: http://alexrabe.de/?page_id=80
 Description: A NextGENeration Photo Gallery for WordPress
 Author: Alex Rabe
-Version: 1.9.2
+Version: 1.9.3
 
 Author URI: http://alexrabe.de/
 
-Copyright 2007-2011 by Alex Rabe & NextGEN DEV-Team
+Copyright 2007-2012 by Alex Rabe & NextGEN DEV-Team
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -34,9 +34,9 @@ if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { die('You 
 if (!class_exists('nggLoader')) {
 class nggLoader {
 	
-	var $version     = '1.9.2';
+	var $version     = '1.9.3';
 	var $dbversion   = '1.8.0';
-	var $minimum_WP   = '3.1';
+	var $minimum_WP  = '3.2';
 	var $donators    = 'http://nextgen.boelinger.com/donators.php';
 	var $options     = '';
 	var $manage_page;
@@ -362,7 +362,7 @@ class nggLoader {
 			wp_enqueue_script('swfobject', NGGALLERY_URLPATH .'admin/js/swfobject.js', FALSE, '2.2');
         else {
             wp_register_script('jquery-cycle', NGGALLERY_URLPATH .'js/jquery.cycle.all.min.js', array('jquery'), '2.9995');
-            wp_enqueue_script('ngg-slideshow', NGGALLERY_URLPATH .'js/ngg.slideshow.min.js', array('jquery-cycle'), '1.05'); 
+            wp_enqueue_script('ngg-slideshow', NGGALLERY_URLPATH .'js/ngg.slideshow.min.js', array('jquery-cycle'), '1.06'); 
                     
         }   
             
