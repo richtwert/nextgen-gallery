68d1c3c2858e04cacce7c278af433e566b416630
Michael Weichert <michael@photocrati.com>
2012-02-28 18:06:27 -0500

    Fixed problems with image forms not saving changed correctly

--- lib/modules/nextgen_imagebrowser_gallery/class.nextgen_imagebrowser_view.php
+++ lib/modules/nextgen_imagebrowser_gallery/class.nextgen_imagebrowser_view.php
@@ -8,7 +8,7 @@ class C_NextGen_ImageBrowser_View extends C_Base_Gallery_View_Controller
         // Disable the display of E_NOTICE as NextGen's meta parser
         // generates many of these
         $er = error_reporting(E_ALL ^ E_NOTICE);
-        echo nggCreateImageBrowser($this->config->get_images(TRUE));
+        echo nggCreateImageBrowser($this->config->get_images(FALSE, FALSE, TRUE));
         error_reporting($er);
     }
 }
