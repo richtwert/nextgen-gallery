cf8d1f2bb4715fd7378ecc7333170502ad756405
Elia Sarti <elia@photocrati.com>
2012-04-05 18:54:57 +0100

    Updates required for latest Pope changes.

--- modules/lightbox/module.lightbox.php
+++ modules/lightbox/module.lightbox.php
@@ -82,7 +82,7 @@ class M_Lightbox extends C_Base_Module
     
     function _register_adapters()
     {
-        $this->_registry->add_adapter(
+        $this->_get_registry()->add_adapter(
             'I_Lightbox_Library',
             'A_JQuery_Lightbox_Library'
         );
