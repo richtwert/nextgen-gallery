12db3532046f350349ee512741a4075612218be3
Elia Sarti <elia@photocrati.com>
2012-04-05 18:54:27 +0100

    Updates required for latest Pope changes.

--- modules/thickbox/module.thickbox.php
+++ modules/thickbox/module.thickbox.php
@@ -29,7 +29,7 @@ class M_Thickbox extends C_Base_Module
     
     function _register_adapters()
     {
-        $this->_registry->add_adapter('I_Lightbox_Library', 'A_Thickbox_Lightbox_Library');
+        $this->_get_registry()->add_adapter('I_Lightbox_Library', 'A_Thickbox_Lightbox_Library');
     }
 }
 
