c9cf7f8582774034273404170bbe28498ca508ed
Elia Sarti <elia@photocrati.com>
2012-04-11 13:59:33 +0100

    Updates to adapt to new URL rewriting changes to support PATHINFO nice URLs.
    The code now uses new functions to generate routing URIs and patterns.

--- modules/resource_loader/module.resource_loader.php
+++ modules/resource_loader/module.resource_loader.php
@@ -9,20 +9,8 @@
 
 define(
     'PHOTOCRATI_GALLERY_MOD_RESOURCE_LOADER_ROUTING_PATTERN',
-    "/\/resource_loader\/?([^\?]*)/"
+    photocrati_gallery_plugin_routing_pattern('resource_loader')
 );
-
-define('PHOTOCRATI_GALLERY_MOD_RESOURCE_LOAD_DYNAMIC_STYLES_URL', path_join(
-    site_url(), 'resource_loader/dynamic_styles'
-));
-
-define('PHOTOCRATI_GALLERY_MOD_RESOURCE_LOAD_DYNAMIC_SCRIPTS_URL', path_join(
-    site_url(), 'resource_loader/dynamic_scripts'
-));
-
-define('PHOTOCRATI_GALLERY_MOD_RESOURCE_LOADER_ID', hash('md5', photocrati_gallery_plugin_serialize(array(
-    $_SERVER['REQUEST_URI'], $_REQUEST
-))));
 
 
 class M_Resource_Loader extends C_Base_Module
