6be00c26ce722beebb37dcbb6c0cfe10bcec53a1
Michael Weichert <michael@photocrati.com>
2012-07-11 14:04:53 -0400

    Made ngglegacy completely self-contained within a module
    --HG--
    branch : alternative-data-and-storage-layers-using-datamapper
    rename : lang/nggallery-de_DE.mo => products/photocrati_nextgen/modules/ngglegacy/lang/nggallery-de_DE.mo
    rename : lang/nggallery-de_DE.po => products/photocrati_nextgen/modules/ngglegacy/lang/nggallery-de_DE.po
    rename : lang/nggallery.pot => products/photocrati_nextgen/modules/ngglegacy/lang/nggallery.pot

--- templates/requirements_not_met.php
+++
@@ -1,9 +0,0 @@
-<div id="message" class="error">
-	<h3><?php _e('NextGEN Gallery Requirements Not Met', PHOTOCRATI_GALLERY_I8N_DOMAIN) ?></h3>
-	<p>
-		<ul>
-			<li><?php print_f(__('WordPress %s or higher', $this->minimum_wordpress_version)) ?></li>
-			<li><?php print_f(__('PHP Memory limit of %dMB or higher', $this->$minimum_memory_limit)) ?></li>
-		</ul>
-	</p>
-</div>
