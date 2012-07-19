c54d8d475dbb0133063ad81ba6ec8fec04bdcee0
Michael Weichert <michael@photocrati.com>
2012-07-19 17:23:52 -0400

    Removed factory method for deprecated 'attached_gallery_image'
    --HG--
    branch : alternative-data-and-storage-layers-using-datamapper

--- products/photocrati_nextgen/modules/attach_to_post/adapter.attached_gallery_factory.php
+++ products/photocrati_nextgen/modules/attach_to_post/adapter.attached_gallery_factory.php
@@ -6,10 +6,4 @@ class A_Attached_Gallery_Factory extends Mixin
     {
         return new C_Attached_Gallery($properties, $context);
     }
-    
-    
-    function attached_gallery_image($properties=array(), $context=FALSE)
-    {
-        return new C_Attached_Gallery_Image($properties, $context);
-    }
 }
