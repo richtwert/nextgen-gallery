fc93bf262007cac1f7fe960095b110de05c05bbf
Michael Weichert <michael@photocrati.com>
2012-08-16 23:12:51 -0400

    Added some formatting to the Global Options JS file
    --HG--
    branch : other-options-tabs

--- products/photocrati_nextgen/modules/nextgen_settings/js/nextgen_settings_page.js
+++ products/photocrati_nextgen/modules/nextgen_settings/js/nextgen_settings_page.js
@@ -1,35 +1,6 @@
 jQuery(function($){
-	$('#watermark_colorpicker').farbtastic('#font_color');
-
 	// Activate accordions
 	$('.accordion').accordion({ clearStyle: true, autoHeight: false });
-
-	// When the lightbox library is changed, display it's properties
-	$('#lightbox_library').change(function(){
-		var selected = $(this).find(':selected');
-		$('#lightbox_library_code').val(selected.attr('code'));
-		$('#lightbox_library_stylesheets').val(selected.attr('css_stylesheets'));
-		$('#lightbox_library_scripts').val(selected.attr('scripts'));
-	}).change();
-
-	// Configure the watermark customization link
-	$('#watermark_customization').attr('rel', 'watermark_'+$('#watermark_source').val()+'_source');
-
-	// When the selected stylesheet changes, fetch it's contents
-	$('#activated_stylesheet').change(function(){
-		var selected = $(this).find(':selected');
-		var data = {
-			action:		'get_stylesheet_contents',
-			cssfile:	selected.val()
-		};
-		$.post(photocrati_ajax_url, data, function(res){
-			if (typeof res !== 'object') res = JSON.parse(res);
-			$('#cssfile_contents').val(res.error ? res.error : res.contents);
-			var status = $('#writable_identicator');
-			if (res.writable) status.text(status.attr('writable_label'));
-			else status.text(status.attr('readonly_label'));
-		});
-	}).change();
 
 	// Toggle the advanced settings
 	$('.advanced_toggle_link').click(function(e){
@@ -47,4 +18,40 @@ jQuery(function($){
 			}
 		});
 	});
+
+	/**** LIGHTBOX EFFECT TAB ****/
+
+	// When the lightbox library is changed, display it's properties
+	$('#lightbox_library').change(function(){
+		var selected = $(this).find(':selected');
+		$('#lightbox_library_code').val(selected.attr('code'));
+		$('#lightbox_library_stylesheets').val(selected.attr('css_stylesheets'));
+		$('#lightbox_library_scripts').val(selected.attr('scripts'));
+	}).change();
+
+	/**** WATERMARK TAB ****/
+
+	// Activate the color picker for the Watermark font color
+	$('#watermark_colorpicker').farbtastic('#font_color');
+
+	// Configure the watermark customization link
+	$('#watermark_customization').attr('rel', 'watermark_'+$('#watermark_source').val()+'_source');
+
+	/**** STYLES TAB ****/
+
+	// When the selected stylesheet changes, fetch it's contents
+	$('#activated_stylesheet').change(function(){
+		var selected = $(this).find(':selected');
+		var data = {
+			action:		'get_stylesheet_contents',
+			cssfile:	selected.val()
+		};
+		$.post(photocrati_ajax_url, data, function(res){
+			if (typeof res !== 'object') res = JSON.parse(res);
+			$('#cssfile_contents').val(res.error ? res.error : res.contents);
+			var status = $('#writable_identicator');
+			if (res.writable) status.text(status.attr('writable_label'));
+			else status.text(status.attr('readonly_label'));
+		});
+	}).change();
 });
