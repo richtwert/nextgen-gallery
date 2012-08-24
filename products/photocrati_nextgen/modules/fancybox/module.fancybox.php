b4f933bfdb5e2dffb395fb4505b2ce8d5ef573e1
Benjamin Owens <benjamin@photocrati.com>
2012-08-24 13:43:48 -0700

    Refactored fancybox lightbox library
    --HG--
    branch : lightbox_settings_refactor
    rename : products/photocrati_nextgen/modules/fancybox/adapter.fancybox_lightbox_library.php => products/photocrati_nextgen/modules/fancybox/adapter.fancybox_library_activation.php

--- products/photocrati_nextgen/modules/fancybox/module.fancybox.php
+++ products/photocrati_nextgen/modules/fancybox/module.fancybox.php
@@ -8,17 +8,22 @@
 
 define('PHOTOCRATI_GALLERY_FANCYBOX_JS_URL', path_join(
         PHOTOCRATI_GALLERY_MODULE_URL,
-        basename(dirname(__FILE__))).'/static/fancybox/jquery.fancybox-1.3.4.pack.js'
+        basename(dirname(__FILE__))) . '/static/fancybox/jquery.fancybox-1.3.4.pack.js'
+);
+
+define('PHOTOCRATI_GALLERY_FANCYBOXY_JS_INIT_URL', path_join(
+    PHOTOCRATI_GALLERY_MODULE_URL,
+    basename(dirname(__FILE__))) . '/static/fancybox/nextgen_fancybox_init.js'
 );
 
 define('PHOTOCRATI_GALLERY_JQUERY_EASING_JS_URL', path_join(
         PHOTOCRATI_GALLERY_MODULE_URL,
-        basename(dirname(__FILE__))).'/static/fancybox/jquery.easing-1.3.pack.js'
+        basename(dirname(__FILE__))) . '/static/fancybox/jquery.easing-1.3.pack.js'
 );
 
 define('PHOTOCRATI_GALLERY_JQUERY_FANCYBOX_CSS_URL', path_join(
         PHOTOCRATI_GALLERY_MODULE_URL,
-        basename(dirname(__FILE__)).'/static/fancybox/jquery.fancybox-1.3.4.css'
+        basename(dirname(__FILE__)) . '/static/fancybox/jquery.fancybox-1.3.4.css'
 ));
 
 class M_Fancybox extends C_Base_Module
@@ -36,34 +41,9 @@ class M_Fancybox extends C_Base_Module
         );
     }
 
-    function _register_hooks()
-    {
-        wp_register_script(
-            'fancybox',
-            PHOTOCRATI_GALLERY_FANCYBOX_JS_URL,
-            array('jquery.easing'),
-            '1.3.4'
-        );
-
-        wp_register_script(
-            'jquery.easing',
-            PHOTOCRATI_GALLERY_JQUERY_EASING_JS_URL,
-            array('jquery'),
-            '1.3'
-        );
-
-        wp_register_style(
-            'fancybox',
-            PHOTOCRATI_GALLERY_JQUERY_FANCYBOX_CSS_URL,
-            array(),
-            '1.3.4'
-        );
-    }
-
-
     function _register_adapters()
     {
-        $this->get_registry()->add_adapter('I_Lightbox_Library', 'A_Fancybox_Lightbox_Library');
+        $this->get_registry()->add_adapter('I_NextGen_Activator', 'A_Fancybox_Library_Activation');
     }
 }
 
