13795d32ec2a8d8e65b1316b67940f13530d9b4d
Elia Sarti <elia@photocrati.com>
2012-09-04 16:39:35 +0100

    Adjust rendering for slideshow gallery to ensure a list of image tags is shown by default for SEO purposes.
    Then the image tags are transformed dynamically to a slideshow using jQuery Cycle.
    
    --HG--
    branch : gallery-basic-slideshow

--- products/photocrati_nextgen/modules/nextgen_basic_slideshow/templates/nextgen_basic_slideshow.php
+++ products/photocrati_nextgen/modules/nextgen_basic_slideshow/templates/nextgen_basic_slideshow.php
@@ -1,6 +1,60 @@
 <?php
 $anchor = 'ngg-slideshow-' . $displayed_gallery_id . '-' . $current_page;
+$aspect_ratio = $gallery_width / $gallery_height;
 ?>
+	
+	<!-- Images -->
+	<div
+		class="ngg-slideshow-image-list ngg-slideshow-nojs"
+		id="<?php echo_h($anchor)?>-image-list">
+	<?php for ($i=0; $i<count($images); $i++): ?>
+        <?php 
+        
+        $image = $images[$i];
+        $image_size = $storage->get_original_dimensions($image);
+        
+        if ($image_size == null) {
+        	$image_size['width'] = $image->meta_data['width'];
+        	$image_size['height'] = $image->meta_data['height'];
+        }
+
+        if (isset($image->hidden) && $image->hidden) {
+          $image->style = 'style="display: none;"';
+        }
+        else {
+        	$image->style = '';
+        }
+        
+        $image_ratio = $image_size['width'] / $image_size['height'];
+        
+        if ($image_ratio > $aspect_ratio) {
+        	if ($image_size['width'] > $gallery_width) {
+        		$image_size['width'] = $gallery_width;
+        		$image_size['height'] = (int) round($gallery_width / $image_ratio);
+        	}
+        }
+        else {
+        	if ($image_size['height'] > $gallery_height) {
+        		$image_size['width'] = (int) round($gallery_height * $image_ratio);
+        		$image_size['height'] = $gallery_height;
+        	}
+        }
+        
+        ?>
+
+        <div id="ngg-image-<?php echo_h($i)?>" class="ngg-gallery-slideshow-image" <?php print $image->style; ?>>
+            <img
+                title="<?php echo esc_attr($image->description)?>"
+                alt="<?php echo esc_attr($image->alttext)?>"
+                src="<?php echo esc_attr($storage->get_image_url($image))?>"
+                width="<?php echo esc_attr($image_size['width'])?>"
+                height="<?php echo esc_attr($image_size['height'])?>"
+            />
+        </div>
+
+	<?php endfor ?>
+	</div>
+	
 <div
 	class="ngg-galleryoverview"
 	id="<?php echo_h($anchor)?>"
@@ -20,9 +74,10 @@ $anchor = 'ngg-slideshow-' . $displayed_gallery_id . '-' . $current_page;
 	<div class="ngg-clear"></div>
 	<?php endif ?>
 </div>
-<script type="text/javascript" defer="defer">
+<script type="text/javascript">
+jQuery('#<?php echo_h($anchor)?>-image-list').hide().removeClass('ngg-slideshow-nojs');
 jQuery(document).ready(function(){ 
-	jQuery('#<?php echo_h($anchor)?>').nggSlideshow({
+	jQuery('#<?php echo_h($anchor)?>').nggShowSlideshow({
 		id: '<?php echo_h($displayed_gallery_id)?>',
 		fx: '<?php echo_h($cycle_effect)?>',
 		width: <?php echo_h($gallery_width)?>,
