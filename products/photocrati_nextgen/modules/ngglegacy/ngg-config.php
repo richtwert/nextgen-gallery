6d0cbb080671c339e006197160bf14a069d912e1
Michael Weichert <michael@photocrati.com>
2012-09-07 08:34:37 -0400

    Adjusted ngg-config.php to work in a Pope environment

--- products/photocrati_nextgen/modules/ngglegacy/ngg-config.php
+++ products/photocrati_nextgen/modules/ngglegacy/ngg-config.php
@@ -8,20 +8,25 @@
  * @package WordPress
  * @since Version 2.6
  */
- 
+
 /** Define the server path to the file wp-config here, if you placed WP-CONTENT outside the classic file structure */
 
-$path  = ''; // It should be end with a trailing slash    
+$path  = ''; // It should be end with a trailing slash
 
 /** That's all, stop editing from here **/
 
 if ( !defined('WP_LOAD_PATH') ) {
 
-	/** classic root path if wp-content and plugins is below wp-config.php */
-	$classic_root = dirname(dirname(dirname(dirname(__FILE__)))) . '/' ;
-	
-	if (file_exists( $classic_root . 'wp-load.php') )
-		define( 'WP_LOAD_PATH', $classic_root);
+	// classic root path if wp-content and plugins is below wp-config.php
+	$legacy_root = dirname(dirname(dirname(dirname(__FILE__)))) . '/' ;
+
+	// root path when ngglegacy is embedded as a Pope module
+	$pope_root = dirname(dirname(dirname(dirname($legacy_root)))) . '/';
+
+	if (file_exists( $legacy_root . 'wp-load.php') )
+		define( 'WP_LOAD_PATH', $legacy_root);
+	elseif (file_exists( $pope_root.'wp-load.php' ) )
+		define( 'WP_LOAD_PATH', $pope_root );
 	else
 		if (file_exists( $path . 'wp-load.php') )
 			define( 'WP_LOAD_PATH', $path);
