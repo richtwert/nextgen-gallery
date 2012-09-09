994839f4104febe886a289ab6c215600b23b1060
Michael Weichert <michael@photocrati.com>
2012-09-09 17:07:54 -0400

    Fixed a problem with iframely being applied whether a page was loaded in an iframe or not. As well, started implementation of Display Settings in attach to post interface

--- products/photocrati_nextgen/modules/gallery_display/static/iframely.js
+++ products/photocrati_nextgen/modules/gallery_display/static/iframely.js
@@ -1,12 +1,18 @@
 jQuery(function($){
 
-	// Concentrate only on the content of the page
-	$('#wpwrap').html($('#wpbody').html($('#wpbody-content').html($('#ngg_page_content'))));
+	// Is this page within an iframe?
+	if (window.frameElement) {
+		// For CSS purposes, we identify this page as being iframely
+		$('html').attr('id', 'iframely');
 
-	// We need to ensure that any POST operation includes the "attach_to_post"
-	// parameter, to display subsequent clicks in iframely.
-	$('form').each(function(){
-		$(this).append("<input type='hidden' name='attach_to_post' value='1'/>");
-	});
-	parent.resize_attach_to_post_tab(window.frameElement, true);
+		// Concentrate only on the content of the page
+		$('#wpwrap').html($('#wpbody').html($('#wpbody-content').html($('#ngg_page_content'))));
+
+		// We need to ensure that any POST operation includes the "attach_to_post"
+		// parameter, to display subsequent clicks in iframely.
+		$('form').each(function(){
+			$(this).append("<input type='hidden' name='attach_to_post' value='1'/>");
+		});
+		parent.resize_attach_to_post_tab(window.frameElement, true);
+	}
 });
