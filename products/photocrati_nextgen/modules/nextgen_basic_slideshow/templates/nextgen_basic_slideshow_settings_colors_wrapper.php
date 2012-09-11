87d78ad6edded6238fd0e1f2b7498e1d5ebf1359
Benjamin Owens <benjamin@photocrati.com>
2012-09-11 16:36:28 -0700

    committing current progress
    --HG--
    branch : slideshow_settings

--- products/photocrati_nextgen/modules/nextgen_basic_slideshow/templates/nextgen_basic_slideshow_settings_colors_wrapper.php
+++ products/photocrati_nextgen/modules/nextgen_basic_slideshow/templates/nextgen_basic_slideshow_settings_colors_wrapper.php
@@ -1,7 +1,17 @@
-<tr class="nextgen-settings-slideshow-flash <?php print ($hidden) ? 'hidden' : ''; ?>">
+<tr class="nextgen-settings-slideshow-flash <?php print ($hidden) ? 'hidden' : ''; ?>"
+    id="nextgen-settings-slideshow-colors-wrapper">
     <td colspan='2'>
-        <?php foreach ($output as $td) {
-            print $td;
-        } ?>
+        <table>
+            <tr>
+                <?php $i = 0; ?>
+                <?php foreach ($output as $td) { ?>
+                    <td>
+                        <?php print $td; ?>
+                    </td>
+                    <?php $i++; ?>
+                    <?php if ($i == 2) { print "</tr><tr>"; $i = 0;} ?>
+                <?php } ?>
+            </tr>
+        </table>
     </td>
 </tr>
