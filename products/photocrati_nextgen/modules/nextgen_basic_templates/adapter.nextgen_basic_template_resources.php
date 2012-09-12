ca35496606051f5a9451e5567312f3fbd3e1ee17
Michael Weichert <michael@photocrati.com>
2012-09-12 15:28:32 -0400

    Moved JS for template support to adapter for C_NextGen_Backend_Controller

---
+++ products/photocrati_nextgen/modules/nextgen_basic_templates/adapter.nextgen_basic_template_resources.php
@@ -0,0 +1,41 @@
+<?php
+
+class A_NextGen_Basic_Template_Resources extends Mixin
+{
+	function initialize()
+	{
+		$this->object->add_pre_hook(
+			'enqueue_backend_resources',
+			'Enqueues resources required for NextGEN template widget',
+			__CLASS__,
+			'enqueue_nextgen_basic_template_resources'
+		);
+	}
+
+	function enqueue_nextgen_basic_template_resources()
+	{
+		wp_enqueue_script(
+            'ngg_template_settings',
+            $this->static_url('/js/ngg_template_settings.js'),
+            array('jquery-ui-autocomplete')
+        );
+
+		// feed our autocomplete widget a list of available files
+        $files_list = array();
+        $files_available = $this->object->get_available_templates();
+        foreach ($files_available as $label => $files)
+        {
+            foreach ($files as $file) {
+                $tmp = explode(DIRECTORY_SEPARATOR, $file);
+                $files_list[] = "[{$label}]: " . end($tmp);
+            }
+        }
+
+        $this->object->_add_script_data(
+            'ngg_template_settings',
+            'nextgen_settings_templates_available_files',
+            $files_list,
+            TRUE
+        );
+	}
+}
