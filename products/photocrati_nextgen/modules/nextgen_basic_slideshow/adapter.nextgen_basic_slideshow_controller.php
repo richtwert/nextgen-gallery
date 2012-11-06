e91b3c4ba8653602988b81fbdb3e8ae032a109fa
Elia Sarti <elia@photocrati.com>
2012-11-06 17:15:07 +0000

    Fix for Chrome rendering issue with the "toss" cycle effect in certain themes.

--- products/photocrati_nextgen/modules/nextgen_basic_slideshow/adapter.nextgen_basic_slideshow_controller.php
+++ products/photocrati_nextgen/modules/nextgen_basic_slideshow/adapter.nextgen_basic_slideshow_controller.php
@@ -65,6 +65,8 @@ class A_NextGen_Basic_Slideshow_Controller extends Mixin
 	 */
 	function enqueue_frontend_resources($displayed_gallery)
 	{
+		wp_enqueue_style('nextgen-basic-slideshow-style', $this->static_url('nextgen-basic-slideshow.css'));
+
 		$this->call_parent('enqueue_frontend_resources', $displayed_gallery);
 	}
 
