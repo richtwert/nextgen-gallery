3a1d2454dc288e94d57a4d20293253b1c6dc68dc
Benjamin Owens <benjamin@photocrati.com>
2012-11-27 15:25:33 -0800

    fixing thickbox display

--- products/photocrati_nextgen/modules/thickbox/static/nextgen_thickbox_init.js
+++ products/photocrati_nextgen/modules/thickbox/static/nextgen_thickbox_init.js
@@ -1,4 +1,10 @@
 var thickboxL10n = {
-	loadingAnimation: wp_site_url+'/wp-includes/js/thickbox/loadingAnimation.gif',
-	closeImage:		  wp_site_url+'/wp-includes/js/thickbox/tb-close.png'
+    loadingAnimation: wp_site_url + '/wp-includes/js/thickbox/loadingAnimation.gif',
+    closeImage: wp_site_url + '/wp-includes/js/thickbox/tb-close.png',
+    next: 'Next &gt;',
+    prev: '&lt; Prev',
+    image: 'Image',
+    of: 'of',
+    close: 'Close',
+    noiframes: 'This feature requires inline frames. You have iframes disabled or your browser does not support them.'
 };
