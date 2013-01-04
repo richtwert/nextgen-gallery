94d17a59405cbbbf213619bb975b9f09b8a2f864
Michael Weichert <michael@photocrati.com>
2013-01-04 11:34:52 -0500

    Replaced Hook_Wordpress_URI_Params_Modifier with Hook_WordPress_Include_Post
    --HG--
    branch : url-params-refactor

--- products/photocrati_nextgen/modules/wordpress_routing/adapter.wordpress_routing_app.php
+++ products/photocrati_nextgen/modules/wordpress_routing/adapter.wordpress_routing_app.php
@@ -2,6 +2,20 @@
 
 class A_WordPress_Routing_App extends Mixin
 {
+	function initialize()
+	{
+		// Both set_parameter_value() and remove_parameter() are methods used
+		// to generate urls. When they used to generate url for a post
+		// on the front-page, the link must to be the post/page itself. We use
+		// the 'Hook_WordPress_Include_Post' to achieve that
+        $this->add_post_hook(
+            'set_parameter_value',
+            'Make Wordpress specific URI adjustments',
+            'Hook_WordPress_Include_Post'
+        );
+	}
+
+
 	/**
 	 * Ensures that the parameters in the request uri don't interfere with normal WordPress routing
 
