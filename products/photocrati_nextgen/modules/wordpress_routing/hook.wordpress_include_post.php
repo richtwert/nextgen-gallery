5107dbf0de2c84d6ee5a30aef0bfb1ee25c31e6e
Michael Weichert <michael@photocrati.com>
2013-01-04 11:46:40 -0500

    Forgot to add hook in last commit
    --HG--
    branch : url-params-refactor

---
+++ products/photocrati_nextgen/modules/wordpress_routing/hook.wordpress_include_post.php
@@ -0,0 +1,39 @@
+<?php
+
+class Hook_WordPress_Include_Post extends Hook
+{
+	function set_parameter_value($key, $value, $id=NULL, $use_prefix=FALSE, $url=FALSE)
+	{
+		return $this->_modify_generated_url($url);
+	}
+
+	function remove_parameter($key, $id=NULL, $url=FALSE)
+	{
+		return $this->_modify_generated_url($url);
+	}
+
+
+	function _modify_generated_url($url)
+	{
+		// Get the method's return value, which is the generated url
+		$retval = $this->object->get_method_property(
+			$this->method_called, ExtensibleObject::METHOD_PROPERTY_RETURN_VALUE
+		);
+
+		// Are we generating a custom url, and are we about to display it on the
+		// front page?
+		if (is_front_page() && $url) {
+			$url = post_permalink();
+			if (strpos($retval, $url) === FALSE) {
+				$retval = str_replace(site_url(), $url, $retval);
+				$this->object->set_method_property(
+					$this->method_called,
+					ExtensibleObject::METHOD_PROPERTY_RETURN_VALUE,
+					$retval
+				);
+			}
+		}
+
+		return $retval;
+	}
+}
