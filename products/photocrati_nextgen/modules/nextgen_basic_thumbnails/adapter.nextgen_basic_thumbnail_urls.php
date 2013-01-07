eecbc96417664ebab4437d363057570b77da5fec
Michael Weichert <michael@photocrati.com>
2013-01-07 18:32:18 -0500

    Fixed ngglegacy page parameter
    --HG--
    branch : url-params-refactor

--- products/photocrati_nextgen/modules/nextgen_basic_thumbnails/adapter.nextgen_basic_thumbnail_urls.php
+++ products/photocrati_nextgen/modules/nextgen_basic_thumbnails/adapter.nextgen_basic_thumbnail_urls.php
@@ -23,7 +23,7 @@ class A_NextGen_Basic_Thumbnail_Urls extends Mixin
 	function create_parameter_segment($key, $value, $id, $use_prefix)
 	{
 		if ($key == 'page') {
-			return '/page/'.$value.'/';
+			return 'page/'.$value;
 		}
 		else
 			return $this->call_parent('create_parameter_segment', $key, $value, $id, $use_prefix);
