175a4a53684cab1a716945cbd24d14b6caa1e187
Michael Weichert <michael@photocrati.com>
2013-01-08 10:44:09 -0500

    Fixed wordpress routing problems

--- products/photocrati_nextgen/modules/wordpress_routing/hook.wordpress_include_post.php
+++ products/photocrati_nextgen/modules/wordpress_routing/hook.wordpress_include_post.php
@@ -19,19 +19,49 @@ class Hook_WordPress_Include_Post extends Hook
 
 		// Are we generating a custom url, and are we about to display it on the front page?
 		if ((is_front_page() OR is_home()) && $url) {
-			$url = rtrim(post_permalink(), '/');
-			if (strpos($retval, $url) === FALSE) {
-				$retval = str_replace(site_url(), $url, $retval);
-				if (strpos($retval, 'index.php') === FALSE)
-					$retval = str_replace(site_url(), $this->get_router()->get_base_url(), $retval);
-				$this->object->set_method_property(
-					$this->method_called,
-					ExtensibleObject::METHOD_PROPERTY_RETURN_VALUE,
-					$retval
+			$url_parts	= parse_url($retval);
+			$perm_parts = parse_url($this->get_post_permalink());
+			if (!isset($url_parts['path']))		$url_parts['path'] = '/';
+			if (!isset($perm_parts['path']))	$perm_parts['path']= '/';
+			if (!isset($url_parts['query']))	$url_parts['query'] = '';
+			if (!isset($perm_parts['query']))	$perm_parts['query']= '';
+
+			// Ensure that the path is set correctly
+			$perm_path	= preg_quote($perm_parts['path'], '#');
+			if (!preg_match("#^{$perm_path}#", $url_parts['path'])) {
+				$url_parts['path'] = $this->object->join_paths(
+					$perm_parts['path'],
+					$url_parts['path']
 				);
 			}
+
+			// Ensure that the querystring is set correctly
+			$perm_qs	= preg_quote($perm_parts['query'], '#');
+			if (!preg_match("#&?{$perm_qs}#", $url_parts['query'])) {
+				if ($url_parts['query']) $url_parts['query'] .= '&'.$perm_parts['query'];
+				else $url_parts['query'] = $perm_parts['query'];
+			}
+
+			// Rebuild the url
+			$this->object->set_method_property(
+				$this->method_called,
+				ExtensibleObject::METHOD_PROPERTY_RETURN_VALUE,
+				($retval = $this->object->join_paths(
+					"{$url_parts['scheme']}://{$url_parts['host']}",
+					$url_parts['path'],
+					'?'.$url_parts['query']
+				))
+			);
 		}
 
+		return $retval;
+	}
+
+	function get_post_permalink()
+	{
+		$retval = post_permalink();
+		if (strpos($retval, 'index.php') === FALSE)
+			$retval = str_replace(site_url(), $this->get_router()->get_base_url(), $retval);
 		return $retval;
 	}
 }
