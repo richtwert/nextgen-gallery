9802440e3eae8bfdfbca795f1045b9228233580e
Benjamin Owens <benjamin@photocrati.com>
2013-01-07 16:02:30 -0800

    enable widgets module
    --HG--
    branch : url-params-refactor

--- products/photocrati_nextgen/product.photocrati_nextgen.php
+++ products/photocrati_nextgen/product.photocrati_nextgen.php
@@ -97,9 +97,9 @@ class P_Photocrati_NextGen extends C_Base_Product
         $this->get_registry()->load_module('photocrati-nextgen_basic_singlepic');
         $this->get_registry()->load_module('photocrati-nextgen_basic_tagcloud');
         $this->get_registry()->load_module('photocrati-nextgen_basic_album');
-//
-//        // Provides sidebar widgets
-//        $this->get_registry()->load_module('photocrati-widget');
+
+        // Provides sidebar widgets
+        $this->get_registry()->load_module('photocrati-widget');
 	}
 }
 
