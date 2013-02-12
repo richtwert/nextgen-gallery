659313ed3482a8eaba292985992ba3a82a7818bb
Michael Weichert <michael@photocrati.com>
2013-02-12 09:18:04 -0500

    Fixed problem with long captions not displaying correctly in the sort interface. Fixed sort order for images. Adjusted tinymce toolbar to insert 'nggalbum' shortcode instead of 'album'
    --HG--
    branch : jetpack-fix-1.9.11

--- admin/manage-images.php
+++ admin/manage-images.php
@@ -258,7 +258,9 @@ jQuery(document).ready( function() {
 					<th align="left">
 					<select <?php nggGallery::current_user_can_form( 'NextGEN Edit gallery page id' ); ?>  name="pageid" style="width:95%">
 						<option value="0" ><?php _e('Not linked', 'nggallery') ?></option>
+						<?php $err = error_reporting(0); ?>
 						<?php parent_dropdown(intval($gallery->pageid)); ?>
+						<?php error_reporting($err); ?>
 					</select>
 					</th>
 				</tr>
