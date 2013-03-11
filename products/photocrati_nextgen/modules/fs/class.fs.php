b301bd73cb729484281f14ba13b1f24c84182fd2
Benjamin Owens <benjamin@photocrati.com>
2013-03-11 15:25:17 -0700

    Reverted/fixed previous bugfix

--- products/photocrati_nextgen/modules/fs/class.fs.php
+++ products/photocrati_nextgen/modules/fs/class.fs.php
@@ -166,13 +166,19 @@ class Mixin_Fs_Instance_Methods extends Mixin
 		$this->_flatten_array($params, $segments);
 
 		foreach ($segments as $segment) {
+            if (strpos($segment, '/') === 0)
+                $segment = substr($segment, 1);
 			if (substr($segment, -1) === '/')
                 $segment = substr($segment, 0, -1);
-			if ($segment) $retval[] = $segment;
+			if ($segment)
+                $retval[] = $segment;
 		}
+
 		$retval = implode('/', $retval);
-		if (strpos($retval, '/') != 0 && strpos($retval, 'http') != 0)
-            $retval = '/'.$retval;
+
+		if (strpos($retval, '/') !== 0 && strpos($retval, 'http') !== 0)
+            $retval = '/' . $retval;
+
 		return $retval;
 	}
 
