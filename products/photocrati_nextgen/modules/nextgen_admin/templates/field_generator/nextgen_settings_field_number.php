5a5c582877b6a53d2a8361ae93390155ab767d17
Benjamin Owens <benjamin@photocrati.com>
2013-04-01 13:09:03 -0700

    Number-field adjustment

--- products/photocrati_nextgen/modules/nextgen_admin/templates/field_generator/nextgen_settings_field_number.php
+++ products/photocrati_nextgen/modules/nextgen_admin/templates/field_generator/nextgen_settings_field_number.php
@@ -8,12 +8,13 @@
     </td>
     <td>
         <input type='number'
+               step='any'
                id='<?php print esc_attr("{$display_type_name}_{$name}"); ?>'
                name='<?php print esc_attr("{$display_type_name}[{$name}]"); ?>'
                class='<?php print esc_attr("{$display_type_name}[{$name}]"); ?>'
                <?php if (!empty($placeholder)) { ?>placeholder='<?php print esc_attr($placeholder); ?>'<?php } ?>
-               <?php if (!empty($min)) { ?>min='<?php print esc_attr($min); ?>'<?php } ?>
-               <?php if (!empty($max)) { ?>max='<?php print esc_attr($max); ?>'<?php } ?>
+               <?php if (!empty($min) || $min === 0) { ?>min='<?php print esc_attr($min); ?>'<?php } ?>
+               <?php if (!empty($max) || $max === 0) { ?>max='<?php print esc_attr($max); ?>'<?php } ?>
                value='<?php print esc_attr($value); ?>'/>
     </td>
 </tr>
