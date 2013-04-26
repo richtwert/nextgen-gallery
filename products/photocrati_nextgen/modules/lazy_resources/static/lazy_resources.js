331773a287d26752255cb439271d5ab0d6dbe813
Michael Weichert <michael@photocrati.com>
2013-04-26 10:07:02 -0400

    Improved lazy resources loader. Display types should use 'lazy_resources_loaded' event instead of jQuery's ready event

--- products/photocrati_nextgen/modules/lazy_resources/static/lazy_resources.js
+++ products/photocrati_nextgen/modules/lazy_resources/static/lazy_resources.js
@@ -5,22 +5,36 @@ var Lazy_Resources = {
 	// Enqueues all styles and scripts, using lazy loading
 	enqueue:		function(){
 		jQuery(function($){
-			Lazy_Resources.enqueue_scripts();
-			Lazy_Resources.enqueue_styles();
+
+            // If there are scripts to load, we'll load them first, which will
+            // automatically load the styles as well after the scripts are finished
+            // loading.
+            //
+            // If there are no scripts to load, we then proceed to just load the styles
+            if (Lazy_Resources.script_urls.length > 0)
+                Lazy_Resources.enqueue_scripts();
+            else
+                Lazy_Resources.enqueue_styles();
 		});
 	},
 
 	// Lazy loads all scripts. Must be called after JQuery has initialized
 	// and ready() event has fired
 	enqueue_scripts:	function(){
-		Sid.js(this.script_urls);
+		Sid.js(this.script_urls, function(){
+            Lazy_Resources.enqueue_styles();
+        });
 		this.script_urls = [];
 	},
 
 	// Lazy loads all styles. Must be called after JQuery has initialized
 	// and ready() event has fired
 	enqueue_styles:		function(){
-		Sid.css(this.style_urls);
+		Sid.css(this.style_urls, function(){
+            setTimeout(function(){
+                jQuery(document).trigger('lazy_resources_loaded');
+            }, 0);
+        });
 		this.style_urls = [];
 	}
 };
