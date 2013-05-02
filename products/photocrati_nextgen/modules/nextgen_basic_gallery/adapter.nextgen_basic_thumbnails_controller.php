10b6917fe5306d1f537b000a549b9826b700aca6
Benjamin Owens <benjamin@photocrati.com>
2013-05-02 10:39:44 -0700

    Move pagination CSS to its own file, adjust basic thumbnails images_per_page tooltip & minimum setting

--- products/photocrati_nextgen/modules/nextgen_basic_gallery/adapter.nextgen_basic_thumbnails_controller.php
+++ products/photocrati_nextgen/modules/nextgen_basic_gallery/adapter.nextgen_basic_thumbnails_controller.php
@@ -175,6 +175,7 @@ class A_NextGen_Basic_Thumbnails_Controller extends Mixin_NextGen_Basic_Gallery_
 
         $settings = $this->get_registry()->get_utility('I_Settings_Manager');
         wp_enqueue_style('nggallery', $this->object->get_static_url('ngglegacy#'.$settings->CSSfile));
+        wp_enqueue_style('nextgen_pagination_style', $this->get_static_url('nextgen_pagination#style.css'));
 
         $this->call_parent('enqueue_frontend_resources', $displayed_gallery);
 	}
